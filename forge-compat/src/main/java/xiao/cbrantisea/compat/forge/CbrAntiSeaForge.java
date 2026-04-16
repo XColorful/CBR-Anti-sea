d321a59f558bb3a5f823cf866d00d790ce89fe38
XiaoColorful <XColor_ful@outlook.com>
2026-04-16 14:02:09 +0800

    Update CbrAntiSeaForge.java

--- forge-compat/src/main/java/xiao/cbrantisea/compat/forge/CbrAntiSeaForge.java
+++ forge-compat/src/main/java/xiao/cbrantisea/compat/forge/CbrAntiSeaForge.java
@@ -4,14 +4,15 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.common.Mod;
 import xiao.battleroyale.api.common.McSide;
+import xiao.cbrantisea.CbrAntiSea;
 
-@Mod(xiao.cbrantisea.CbrAntiSea.MOD_ID)
+@Mod(CbrAntiSea.MOD_ID)
 public class CbrAntiSeaForge {
 
     public CbrAntiSeaForge() {
         Dist dist = FMLLoader.getDist();
         McSide mcSide = dist.isClient() ? McSide.CLIENT : McSide.DEDICATED_SERVER;
 
-        xiao.cbrantisea.CbrAntiSea.init(mcSide);
+        CbrAntiSea.init(mcSide);
     }
 }
