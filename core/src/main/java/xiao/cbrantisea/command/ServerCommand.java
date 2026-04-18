d1966e0846db79c5091c63e9e67d54fdd95157d3
XiaoColorful <XColor_ful@outlook.com>
2026-04-18 11:37:30 +0800

    Update ServerCommand.java

--- core/src/main/java/xiao/cbrantisea/command/ServerCommand.java
+++ core/src/main/java/xiao/cbrantisea/command/ServerCommand.java
@@ -8,14 +8,11 @@ import xiao.battleroyale.api.minecraft.CommandLevel;
 import xiao.cbrantisea.command.sub.AntiseaCommand;
 import xiao.cbrantisea.command.sub.TempCommand;
 
-import static xiao.battleroyale.command.CommandArg.MOD_ID;
-import static xiao.battleroyale.command.CommandArg.MOD_NAME_SHORT;
-
 public class ServerCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(get(MOD_ID));
-        dispatcher.register(get(MOD_NAME_SHORT));
+        dispatcher.register(get(xiao.battleroyale.command.CommandArg.MOD_ID));
+        dispatcher.register(get(xiao.battleroyale.command.CommandArg.MOD_NAME_SHORT));
     }
 
     public static LiteralArgumentBuilder<CommandSourceStack> get(String rootName) {
